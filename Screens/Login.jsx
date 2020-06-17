cc5e4025aa1bd0c760018c8b6b9dd69a1d09ea3c
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-17 18:59:00 +0500

    boilerplate

---
+++ Screens/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+const Login = () => {
+  return (
+      <SafeAreaView>
+        <Text>Login</Text>
+      </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default Login;
---
+++ Screens/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+const Signup = () => {
+  return (
+      <SafeAreaView>
+        <Text>Signup</Text>
+      </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default Signup;
