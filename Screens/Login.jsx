62978473a0efdb3a8eef2dc98265390e645be6ca
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-18 17:05:29 +0500

    done

--- Screens/Login.jsx
+++ Screens/Login.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, Button } from 'react-native';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux'
-import { loginUser } from '../Redux/actions/authActions'
+import { loginUser, removeUser } from '../Redux/actions/authActions'
 
-const user = { name: 'Mansoor' }
+const user1 = { name: 'Mansoor' }
 
 
 const Login = (props) => {
   const user = useSelector(state => state.user, shallowEqual)
-  console.log('user', useSelector(state => state))
   const dispatch = useDispatch();
   return (
     <SafeAreaView>
-      <Button title="Text" onPress={() => dispatch(loginUser(user))} />
-      <Text style={styles.TextStyle}>Login</Text>
+      <Button title="Text" onPress={() => dispatch(loginUser(user1))} />
+      <Text style={styles.TextStyle}>Login {user?.name}</Text>
     </SafeAreaView>
   )
 };
