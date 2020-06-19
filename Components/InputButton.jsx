12e74961661c7d8082f6097a80a3397a76aac271
myasirkha575 <myasirkhan575@gmail.com>
2020-06-20 00:19:46 +0500

    Login page dummy

---
+++ Components/InputButton.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import {Button, StyleSheet, SafeAreaView} from 'react-native';
+
+const InputButton = props => {
+  return (
+    <SafeAreaView>
+      <Text>Login</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+    
+});
--- Navigation/AppNavigator.jsx
+++ Navigation/AppNavigator.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Login, Signup } from '../Screens'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Login, Signup} from '../Screens';
 
 const Stack = createStackNavigator();
 
 function MyStack() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-            >
-                <Stack.Screen name="Home" component={Login} />
-                <Stack.Screen name="Notifications" component={Signup} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Signup">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default MyStack
+export default MyStack;
