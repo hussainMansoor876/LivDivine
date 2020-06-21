c5b7d7dabb7c59f79963e6c6c63cddf5bdd810a6
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-21 22:08:04 +0500

    done

--- Components/LoginForm.jsx
+++ Components/LoginForm.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState, createRef } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginUser, removeUser } from '../Redux/actions/authActions';
 import { Icon, Input, Button } from 'react-native-elements'
 import { loginStyles } from '../styles'
 
+
 const user1 = { name: 'Mansoor Hussain' };
 
 const LoginForm = (props) => {
+    const emailInput = React.createRef();
     const user = useSelector(state => state.authReducer.user);
     const dispatch = useDispatch();
+    const [email, setEmail] = useState('')
+    const [errMsg, setErrMsg] = useState('')
+
+    const validate = () => {
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (reg.test(email) === false) {
+            setErrMsg('Please input Valid Email!')
+            emailInput.current.shake()
+        }
+        else {
+            console.log("Email is Correct");
+        }
+    }
     return (
         <View style={loginStyles.loginView}>
             <Input
                 placeholder="Email"
                 inputContainerStyle={loginStyles.inputLogin}
+                onChangeText={e => setEmail(e)}
+                onBlur={validate}
+                onFocus={() => setErrMsg('')}
+                ref={emailInput}
+                errorMessage={errMsg}
                 leftIcon={
                     <Icon
                         name='mail'
@@ -38,9 +58,9 @@ const LoginForm = (props) => {
                 }
             />
             <Button title="Login" buttonStyle={loginStyles.loginBtn} onPress={() => dispatch(loginUser(user1))} />
-            <TouchableOpacity >
-        <Text style={loginStyles.forgotPas}>I forgot my Password</Text>
-      </TouchableOpacity>
+            <TouchableOpacity onPress={() => console.log('Hello')}>
+                <Text style={loginStyles.forgotPas}>I forgot my Password</Text>
+            </TouchableOpacity>
         </View>
     );
 };
