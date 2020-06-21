cba5b3b563bf4461ed75e400a6991f489f4aa248
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-21 20:32:34 +0500

    done

--- Screens/Login.jsx
+++ Screens/Login.jsx
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { loginUser, removeUser } from '../Redux/actions/authActions';
 import { Icon, Input, SocialIcon } from 'react-native-elements'
 import { LoginForm } from '../Components'
+import { loginStyles } from '../styles'
 
 const user1 = { name: 'Mansoor Hussain' };
 
@@ -13,64 +14,19 @@ const Login = (props) => {
   const user = useSelector(state => state.authReducer.user);
   const dispatch = useDispatch();
   return (
-    <SafeAreaView style={styles.setFlex}>
+    <SafeAreaView style={loginStyles.setFlex}>
       <LoginForm />
       <TouchableOpacity>
         <Text>I forgot my Password</Text>
       </TouchableOpacity>
-      <Text style={styles.connect}>Or Connect With</Text>
-      <View style={styles.social}>
+      <Text style={loginStyles.connect}>Or Connect With</Text>
+      <View style={loginStyles.social}>
         <SocialIcon title="Facebook" button type="facebook" />
         <SocialIcon title="Google" button type="google" />
       </View>
-      <Text style={styles.dhaa}>Don't Have an Account</Text>
+      <Text style={loginStyles.dhaa}>Don't Have an Account</Text>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  setFlex: {
-    flex: 1
-  },
-  loginView: {
-    flex: 1,
-    marginRight: 10,
-    marginLeft: 10,
-    paddingTop: 20
-  },
-  TextStyle: {
-    textAlign: 'center',
-    fontSize: 24,
-  },
-  connect: {
-    marginTop: 20,
-    textAlign: 'center',
-    color: 'grey',
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  dhaa: {
-    marginTop: 20,
-    textAlign: 'center',
-    color: 'orange',
-    fontWeight: 'normal',
-    fontSize: 20,
-  },
-  social: {
-    flexDirection: 'row',
-  },
-  inputLogin: {
-    borderColor: '#000000',
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingLeft: 15,
-  },
-  loginBtn: {
-    borderRadius: 8,
-    width: '96%',
-    marginLeft: '2%',
-    height: 46
-  }
-});
 
 export default Login;
