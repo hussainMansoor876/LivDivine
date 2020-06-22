0fb0817334e7ece025824e9020d4105fbf692b00
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-22 17:00:13 +0500

    done

--- Navigation/AppNavigator.jsx
+++ Navigation/AppNavigator.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {Login, Signup} from '../Screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Login, Signup, Advisors, Categories, FavoriteAdvisors, Home, Journey, Settings } from '../Screens';
 
 const Stack = createStackNavigator();
 
@@ -13,8 +13,14 @@ function MyStack() {
           headerShown: false,
         }}
         initialRouteName="Signup">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="signup" component={Signup} />
+        <Stack.Screen name="advisors" component={Advisors} />
+        <Stack.Screen name="categories" component={Categories} />
+        <Stack.Screen name="favorite" component={FavoriteAdvisors} />
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="journey" component={Journey} />
+        <Stack.Screen name="settings" component={Settings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
