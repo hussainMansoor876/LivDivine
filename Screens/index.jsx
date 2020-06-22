5a7d4902307f7ecfd0389d8636ded771751f1095
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-22 16:57:43 +0500

    done

--- Screens/index.jsx
+++ Screens/index.jsx
@@ -1,7 +1,19 @@
 import Login from './Login';
 import Signup from './Signup';
+import Advisors from './Advisors/Advisors'
+import Categories from './Categories/Categories'
+import FavoriteAdvisors from './FavoriteAdvisors/FavoriteAdvisors'
+import Home from './Home/Home'
+import Journey from './Journey/Journey'
+import Settings from './Settings/Settings'
 
 export {
     Login,
-    Signup
+    Signup,
+    Advisors,
+    Categories,
+    FavoriteAdvisors,
+    Home,
+    Journey,
+    Settings
 }
