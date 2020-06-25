798a33aad3ed3afc26881082f6d484686ba322c9
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-25 19:48:42 +0500

    done

--- Navigation/AppNavigator.jsx
+++ Navigation/AppNavigator.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Login, Signup, Advisors, Categories, FavoriteAdvisors, Home, Journey, Settings } from '../Screens';
 
 const Stack = createStackNavigator();
