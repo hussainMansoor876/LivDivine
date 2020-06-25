188b35757793ad8a7ae8a143b31321228a54bc59
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-25 19:32:41 +0500

    done

--- android/app/src/main/java/com/purpleoccean/MainActivity.java
+++ android/app/src/main/java/com/purpleoccean/MainActivity.java
@@ -3,6 +3,9 @@ import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -14,5 +17,15 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     SplashScreen.show(this);
     return "PurpleOccean";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
