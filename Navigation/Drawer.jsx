4e1056a2ddf1c2f6500c727c02324957696de315
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-25 20:40:01 +0500

    done

--- Navigation/Drawer.jsx
+++ Navigation/Drawer.jsx
@@ -1,20 +1,46 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Dimensions } from 'react-native';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { Advisors, Categories, FavoriteAdvisors, Home, Journey, Settings } from '../Screens';
+import { createAppContainer } from 'react-navigation'
 
-const Drawer = createDrawerNavigator();
+const DrawerNavigatorExample = createDrawerNavigator(
+    {
+      //Drawer Optons and indexing
+      NavScreen1: {
+        screen: Home,
+        navigationOptions: {
+          drawerLabel: 'Demo Screen 1',
+        },
+      },
+      NavScreen2: {
+        screen: Settings,
+        navigationOptions: {
+          drawerLabel: 'Demo Screen 2',
+        },
+      }
+    },
+    {
+      //For the Custom sidebar menu we have to provide our CustomSidebarMenu
+    //   contentComponent: CustomSidebarMenu,
+      //Sidebar width
+      drawerWidth: Dimensions.get('window').width - 130,
+    }
+  );
 
-const DrawerScreen = () => {
-    return (
-        <NavigationContainer>
-            <Drawer.Navigator initialRouteName="Home">
-                <Drawer.Screen name="Home" component={Home} />
-                <Drawer.Screen name="Settings" component={Settings} />
-            </Drawer.Navigator>
-        </NavigationContainer>
-    );
-}
+// const DrawerScreen = () => {
+//     return (
+//         <NavigationContainer>
+//             <Drawer.Navigator 
+//             initialRouteName="Home"
 
-export default DrawerScreen;
+//             >
+//                 <Drawer.Screen name="Home" component={Home} />
+//                 <Drawer.Screen name="Settings" component={Settings} />
+//             </Drawer.Navigator>
+//         </NavigationContainer>
+//     );
+// }
+
+export default createAppContainer(DrawerNavigatorExample);
