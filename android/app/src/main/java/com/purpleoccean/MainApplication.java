0907a51978562d2676a99fc63aece0929703bc74
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-28 02:46:52 +0500

    done

--- android/app/src/main/java/com/purpleoccean/MainApplication.java
+++ android/app/src/main/java/com/purpleoccean/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          packages.add(new RNGoogleSigninPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
