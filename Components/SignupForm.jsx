7d3f1282596187576a5fa854a2e629f0bdb7f4aa
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-06-28 19:38:21 +0500

    done

--- Components/SignupForm.jsx
+++ Components/SignupForm.jsx
@@ -15,8 +15,14 @@ const SignupForm = (props) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [errMsg, setErrMsg] = useState('')
+    const [state, setState] = useState({
+        userName: '',
+        email: '',
+        password: '',
+        confirmPass: ''
+    })
 
-    const validate = () => {
+    const validateSignup = () => {
         let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         if (reg.test(email) === false) {
             setErrMsg('Please input Valid Email!')
@@ -25,6 +31,13 @@ const SignupForm = (props) => {
             console.log("Email is Correct");
         }
     }
+
+    const updateField = (name, value) => {
+        setState({
+            ...state,
+            [name]: value
+        })
+    }
     return (
         <View style={loginStyles.loginView}>
             <Text style={{ textAlign: 'center', fontSize: 16, marginBottom: 20 }}>Or create an account</Text>
@@ -32,7 +45,9 @@ const SignupForm = (props) => {
                 placeholder="Full Name"
                 secureTextEntry={true}
                 inputContainerStyle={loginStyles.inputLogin}
-                onChangeText={e => setPassword(e)}
+                onChangeText={e => updateField('userName', e)}
+                name="userName"
+                value={state.userName}
                 leftIcon={
                     <Icon
                         name='lock'
@@ -45,8 +60,9 @@ const SignupForm = (props) => {
             <Input
                 placeholder="Email"
                 inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: errMsg ? 'red' : '#000000' }}
-                onChangeText={e => setEmail(e)}
-                // onBlur={validate}
+                onChangeText={e => updateField('email', e)}
+                name="email"
+                value={state.email}
                 onFocus={() => setErrMsg('')}
                 ref={emailInput}
                 errorMessage={errMsg}
@@ -63,7 +79,9 @@ const SignupForm = (props) => {
                 placeholder="Password"
                 secureTextEntry={true}
                 inputContainerStyle={loginStyles.inputLogin}
-                onChangeText={e => setPassword(e)}
+                onChangeText={e => updateField('password', e)}
+                name="password"
+                value={state.password}
                 leftIcon={
                     <Icon
                         name='lock'
@@ -77,7 +95,9 @@ const SignupForm = (props) => {
                 placeholder="Confirm Password"
                 secureTextEntry={true}
                 inputContainerStyle={loginStyles.inputLogin}
-                onChangeText={e => setPassword(e)}
+                onChangeText={e => updateField('confirmPassword', e)}
+                name="confirmPassword"
+                value={state.confirmPassword}
                 leftIcon={
                     <Icon
                         name='lock'
