926d694ce80a84258d03708f76a324dfa988ea6b
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-07-01 17:17:32 +0500

    done

--- Components/LoginForm.jsx
+++ Components/LoginForm.jsx
@@ -28,21 +28,22 @@ const user1 = { name: 'Mansoor Hussain' };
 
 const LoginForm = (props) => {
     const dispatch = useDispatch();
-    const [errMsg, setErrMsg] = useState('')
-    const [errPass, setErrPass] = useState('')
     const [state, setState] = useState({
         email: '',
         password: '',
+        emailErr: '',
+        passwordErr: '',
+        isLoading: false
     })
 
     const validateLogin = () => {
         const { email, password } = state
         let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         if (reg.test(email) === false) {
-            return setErrMsg('Please input Valid Email!')
+            return updateField({ emailErr: 'Invalid Email!' })
         }
-        if (password.length < 6) {
-            return setErrPass('Password must be alteast 6 characters!')
+        else if (!password.length || password.length < 6) {
+            return updateField({ passwordErr: 'Password length must be 6 Characters!' })
         }
 
         dispatch(loginUser(user1))
@@ -59,12 +60,12 @@ const LoginForm = (props) => {
         <View style={loginStyles.loginView}>
             <Input
                 placeholder="Email"
-                inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: errMsg ? 'red' : '#000000' }}
+                inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.emailErr ? 'red' : '#000000' }}
                 onChangeText={e => updateField({ email: e })}
                 name="email"
                 value={state.email}
-                onFocus={() => setErrMsg('')}
-                errorMessage={errMsg}
+                errorMessage={state.emailErr}
+                onFocus={() => updateField({ emailErr: '' })}
                 leftIcon={
                     <Icon
                         name='mail'
@@ -77,12 +78,12 @@ const LoginForm = (props) => {
             <Input
                 placeholder="Password"
                 secureTextEntry={true}
-                inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: errPass ? 'red' : '#000000' }}
+                inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.passwordErr ? 'red' : '#000000' }}
                 onChangeText={e => updateField({ password: e })}
                 name="password"
                 value={state.password}
-                onFocus={() => setErrPass('')}
-                errorMessage={errPass}
+                errorMessage={state.passwordErr}
+                onFocus={() => updateField({ passwordErr: '' })}
                 leftIcon={
                     <Icon
                         name='lock'
