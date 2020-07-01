e5ae3f0ee1ac04adc5ae3d1bcd965f9ef659e969
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-07-01 17:10:43 +0500

    done

--- Components/SocialLogin.jsx
+++ Components/SocialLogin.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginUser, removeUser } from '../Redux/actions/authActions';
 import { SocialIcon } from 'react-native-elements'
 import { loginStyles } from '../styles'
-import { LoginManager } from 'react-native-fbsdk';
+import { LoginManager, AccessToken, GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
+import { GoogleSignin, statusCodes } from '@react-native-community/google-signin';
+GoogleSignin.configure();
+
 
 const user1 = { name: 'Mansoor Hussain' };
 
@@ -12,15 +15,40 @@ const SocialLogin = (props) => {
     const user = useSelector(state => state.authReducer.user);
     const dispatch = useDispatch();
 
+    // useEffect(async () => {
+    //     try {
+    //         await GoogleSignin.signOut()
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }, [])
+
     const facebookLogin = () => {
         LoginManager.logInWithPermissions(['public_profile', 'email'])
             .then((result) => {
-                console.log('result', result)
                 if (result.isCancelled) {
                     console.log("Login cancelled");
                 }
                 else {
-                    console.log("Login success with permissions: " + result.grantedPermissions.toString())
+                    AccessToken.getCurrentAccessToken()
+                        .then((data) => {
+                            const { accessToken } = data
+                            console.log('data', accessToken)
+
+                            const get_Response_Info = (error, result) => {
+                                if (error) {
+                                    console.log('Error fetching data: ' + error.toString());
+                                } else {
+                                    console.log('result', JSON.stringify(result))
+                                }
+                            };
+                            const processRequest = new GraphRequest(
+                                '/me?fields=name,email,picture.type(large)',
+                                null,
+                                get_Response_Info
+                            );
+                            new GraphRequestManager().addRequest(processRequest).start();
+                        })
                 }
             }
             )
@@ -28,11 +56,30 @@ const SocialLogin = (props) => {
                 console.log('error', error)
             })
     }
+
+
+    const signIn = async () => {
+        try {
+            // await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            console.log('userInfo', userInfo)
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                // operation (e.g. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                // play services not available or outdated
+            } else {
+                // some other error happened
+            }
+        }
+    };
     return (
         <View>
             <View style={loginStyles.social}>
                 <SocialIcon style={loginStyles.socialBtn} onPress={facebookLogin} title="Facebook" button type="facebook" />
-                <SocialIcon style={loginStyles.socialBtn} title="Google" button type="google" />
+                <SocialIcon style={loginStyles.socialBtn} title="Google" onPress={signIn} button type="google" />
             </View>
         </View>
     );
--- Navigation/index.jsx
+++ Navigation/index.jsx
@@ -26,7 +26,7 @@ const Home = (props) => {
     const user = useSelector(state => state.authReducer.user);
     const dispatch = useDispatch();
 
-    if (!user) {
+    if (user) {
         return (
             <Drawer />
         )
