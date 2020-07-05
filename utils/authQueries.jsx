959426f00e8b5f6d44e188d673d4893c40713538
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-07-06 01:56:12 +0500

    done

--- utils/authQueries.jsx
+++ utils/authQueries.jsx
@@ -25,7 +25,34 @@ mutation($email: String!, $password: String!){
   }
 `
 
+const SOCIAL_LOGIN = gql`
+mutation($email: String, $userName: String!, $authType: String!){
+  socialSignUp(
+    email: $email,
+    userName: userName,
+    authType: $authType
+	  isVerified: true){
+     token, message, success, user{
+     id, userName, email, role, image, isVerified, isLogin,authType,title,advisorImage,
+      aboutService, aboutMe
+    }
+  }
+}
+`
+const UPDATE_PASSWORD = gql`
+mutation($email: String, $pasword: String!){
+  updatePassword(email: $email, password: $pasword){
+    token, message, success,user {
+       id, userName, email, role, image, isVerified, isLogin,authType,title,advisorImage,
+      aboutService, aboutMe
+    }
+  }
+}
+`
+
 export {
-    SIGN_UP,
-    LOGIN
+  SIGN_UP,
+  LOGIN,
+  SOCIAL_LOGIN,
+  UPDATE_PASSWORD
 }
--- utils/index.jsx
+++
@@ -1,6 +0,0 @@
-import { LOGIN, SIGN_UP } from './authQueries'
-
-export {
-    LOGIN,
-    SIGN_UP
-}
