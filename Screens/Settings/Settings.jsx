35534a1ceb14b1ee42e2a4b49cfc50510a8ebdfc
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-07-14 22:38:12 +0500

    done

--- Screens/Settings/Settings.jsx
+++ Screens/Settings/Settings.jsx
@@ -21,23 +21,6 @@ const Settings = (props) => {
                 </View>
                 <SettingsForm {...props} />
                 <ChangePassword {...props} />
-                {/* <View>
-                    <Text style={settingsStyles.fields}>User Name</Text>
-                    <Text style={settingsStyles.fields}>Email</Text>
-                    <Text style={settingsStyles.fields}>User ID</Text>
-                    <Text style={settingsStyles.fields}>Change password</Text>
-                    <Text style={settingsStyles.fields}>Retype password</Text>
-                </View>
-                <View style={settingsStyles.header}>
-                    <Text h1 style={settingsStyles.fieldsbold}>MORE INFORMATION</Text>
-                </View>
-                <Text style={settingsStyles.fields}>
-                    Version
-                    </Text>
-                <Text style={settingsStyles.fields}>About us</Text>
-                <Text style={settingsStyles.fields}>Terms of Service</Text>
-                <Text style={settingsStyles.fields}>Privacy Policy</Text>
-                <Text style={settingsStyles.fields}>Purple Ocean website</Text> */}
             </ScrollView>
         </SafeAreaView>
     );
