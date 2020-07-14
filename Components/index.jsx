d1dae32d09045db70d4a7ab120daa69f63562b64
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-07-14 21:39:55 +0500

    done

--- Components/index.jsx
+++ Components/index.jsx
@@ -3,11 +3,13 @@ import SocialLogin from './SocialLogin'
 import SignupForm from './SignupForm'
 import Header from './Header'
 import SettingsForm from './SettingsForm'
+import ChangePassword from './ChangePassword' 
 
 export {
     LoginForm,
     SocialLogin,
     SignupForm,
     Header,
-    SettingsForm
+    SettingsForm,
+    ChangePassword
 }
