db41b31a11fd8892bec85db8f30b35ef9ca774f4
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-07-25 16:21:44 +0500

    done

--- Components/BecomeAdvisorForm.jsx
+++ Components/BecomeAdvisorForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Image, Text, Alert, TouchableOpacity } from 'react-native';
+import { View, Image, Text, Alert, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginUser, removeUser } from '../Redux/actions/authActions';
 import { Icon, Input, Button, CheckBox, Tile } from 'react-native-elements'
@@ -17,6 +17,7 @@ import RNThumbnail from 'react-native-thumbnail';
 import Screen from '../utils/ScreenDimensions'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import { customStyles, labels, videoOptions } from '../utils/constant'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 const BecomeAdvisorForm = (props) => {
@@ -25,6 +26,9 @@ const BecomeAdvisorForm = (props) => {
     const dispatch = useDispatch();
     const [photo, setPhoto] = useState(null)
     const [uploadVideo, setUploadVideo] = useState(null)
+    const [showVideo, setShowVideo] = useState(false)
+    const [isLoadingVideo, setLoading] = useState(true)
+    const [currentTime, setCurretTime] = useState(0)
     const [state, setState] = useState({
         userName: user.userName,
         title: '',
@@ -47,6 +51,17 @@ const BecomeAdvisorForm = (props) => {
                 setPhoto(response)
             }
         })
+    }
+
+    const updateLoading = (e) => {
+        if (currentTime === e.currentTime) {
+            setLoading(true)
+        }
+        else {
+            setLoading(false)
+        }
+
+        setCurretTime(e.currentTime)
     }
 
     const handleChooseVideo = () => {
@@ -144,136 +159,167 @@ const BecomeAdvisorForm = (props) => {
             ...obj
         })
     }
+
+    const goBack = () => {
+        setShowVideo(false)
+        setLoading(true)
+    }
     return (
-        <View style={loginStyles.loginView}>
-            <Spinner
-                visible={state.isLoading}
-                textContent={'Loading...'}
-                textStyle={loginStyles.spinnerTextStyle}
-            />
-            <Text style={{ textAlign: 'center', fontSize: 24, marginBottom: 20, marginTop: 20, textDecorationLine: 'underline' }}>Become Advisor</Text>
-            <StepIndicator
-                customStyles={customStyles}
-                currentPosition={state.currentPosition}
-                labels={labels}
-            />
-            <View style={{ marginTop: 30 }}>
-                {state.currentPosition === 0 ? <View>
-                    <Input
-                        placeholder="Full Name"
-                        inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.userNameErr ? 'red' : '#000000' }}
-                        onChangeText={e => updateField({ userName: e })}
-                        value={state.userName}
-                        errorMessage={state.userNameErr}
-                        onFocus={() => updateField({ userNameErr: '' })}
-                        leftIcon={
-                            <Icon
-                                name='user'
-                                size={24}
-                                color='black'
-                                type='font-awesome'
-                            />
-                        }
+        <SafeAreaView style={loginStyles.setFlex}>
+            {showVideo ? <View style={{ height: Screen.height, backgroundColor: '#000' }}>
+                <TouchableOpacity onPress={goBack} style={AdvisorStyles.leftIcon}>
+                    <Icon
+                        type="font-awesome"
+                        color="#fff"
+                        name="chevron-left"
+                        iconStyle={{ zIndex: 9999 }}
                     />
-                    <Input
-                        placeholder="Title"
-                        inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.titleErr ? 'red' : '#000000' }}
-                        onChangeText={e => updateField({ title: e })}
-                        value={state.title}
-                        errorMessage={state.titleErr}
-                        onFocus={() => updateField({ titleErr: '' })}
-                        leftIcon={
-                            <FontIcon
-                                name='slack'
-                                size={24}
-                                color='black'
-                            />
-                        }
+                </TouchableOpacity>
+                {isLoadingVideo ? <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: Screen.height, position: 'absolute' }}>
+                    <ActivityIndicator
+                        color="#fff"
+                        size="small"
+                        style={AdvisorStyles.ActivityIndicatorStyle}
                     />
-
-                    {photo && (
-                        <Image
-                            source={{ uri: photo.uri }}
-                            style={{ width: 150, height: 150, marginRight: 10, marginLeft: 10, borderRadius: 250 }}
-                        />
-                    )}
-                    <Button title="Choose Photo" buttonStyle={{ ...loginStyles.loginBtn, width: 150 }} onPress={handleChoosePhoto} />
-                </View> : state.currentPosition === 1 ? <View>
-                    <Input
-                        placeholder="About my services"
-                        multiline={true}
-                        numberOfLines={4}
-                        inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.aboutService ? 'red' : '#000000' }}
-                        onChangeText={e => updateField({ aboutService: e })}
-                        value={state.aboutService}
-                        errorMessage={state.aboutServiceErr}
-                        onFocus={() => updateField({ aboutServiceErr: '' })}
-                    />
-                    <Input
-                        placeholder="Aboutme"
-                        multiline={true}
-                        numberOfLines={4}
-                        inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.aboutMe ? 'red' : '#000000' }}
-                        onChangeText={e => updateField({ aboutMe: e })}
-                        value={state.aboutMe}
-                        errorMessage={state.aboutMeErr}
-                        onFocus={() => updateField({ aboutMeErr: '' })}
-                    />
-                </View> : state.currentPosition === 2 ? <View>
-
-                </View> : state.currentPosition === 3 ? <View>
-                    {uploadVideo ? <Video
-                        source={{ uri: uploadVideo.uri }}
-                        style={{ height: Screen.height / 3 }}
-                        resizeMode="contain"
-                        controls={true}
-                        paused={true}
-                        thu
-                    /> : null}
-                    {!photo && (
-                        <View style={{ justifyContent: 'center' }}>
-                            <TouchableOpacity onPress={() => setShowVideo(true)} style={{ height: 230, width: Screen.width }}>
-                                <Image
-                                    source={{ uri: 'https://res.cloudinary.com/dhspait8a/image/upload/v1595100989/cwbwopm3ys9hpkjaajw1.jpg' }}
-                                    style={{ height: 230, width: Screen.width, resizeMode: 'cover' }}
-                                />
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={() => setShowVideo(true)} style={AdvisorStyles.playButton}>
-                                <MaterialIcon style={{ color: '#fff' }} name="play-arrow" size={42} />
-                            </TouchableOpacity>
-                        </View>
-                    )}
-                    <Button title="Record or Choose Video" buttonStyle={{ ...loginStyles.loginBtn, width: 150 }} onPress={handleChooseVideo} />
-                    <Text>Briefly tell potential clients about your service. Make sure that the video and the audio clear and give it your best effort</Text>
-                </View> : state.currentPosition === 4 ? <View>
-
                 </View> : null}
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
-                <Button icon={
-                    <FontIcon
-                        name='arrow-left'
-                        size={24}
-                        color='#fff'
-                    />
-                }
-                    buttonStyle={{ width: 150 }}
-                    disabled={!state.currentPosition}
-                    onPress={() => updateFieldSteps('left')}
+                <Video
+                    source={{ uri: 'https://vjs.zencdn.net/v/oceans.mp4' }}
+                    style={{ marginTop: Screen.height / 4, height: Screen.height / 2, width: Screen.width }}
+                    controls
+                    resizeMode="stretch"
+                    onProgress={updateLoading}
                 />
-                <Button icon={
-                    <FontIcon
-                        name='arrow-right'
-                        size={24}
-                        color='#fff'
+            </View> :
+                <View style={loginStyles.loginView}>
+                    <Spinner
+                        visible={state.isLoading}
+                        textContent={'Loading...'}
+                        textStyle={loginStyles.spinnerTextStyle}
                     />
-                }
-                    buttonStyle={{ width: 150 }}
-                    onPress={() => updateFieldSteps('right')}
-                    disabled={state.currentPosition === 5}
-                />
-            </View>
-        </View>
+                    <Text style={{ textAlign: 'center', fontSize: 24, marginBottom: 20, marginTop: 20, textDecorationLine: 'underline' }}>Become Advisor</Text>
+                    <StepIndicator
+                        customStyles={customStyles}
+                        currentPosition={state.currentPosition}
+                        labels={labels}
+                    />
+                    <View style={{ marginTop: 30 }}>
+                        {state.currentPosition === 0 ? <View>
+                            <Input
+                                placeholder="Full Name"
+                                inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.userNameErr ? 'red' : '#000000' }}
+                                onChangeText={e => updateField({ userName: e })}
+                                value={state.userName}
+                                errorMessage={state.userNameErr}
+                                onFocus={() => updateField({ userNameErr: '' })}
+                                leftIcon={
+                                    <Icon
+                                        name='user'
+                                        size={24}
+                                        color='black'
+                                        type='font-awesome'
+                                    />
+                                }
+                            />
+                            <Input
+                                placeholder="Title"
+                                inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.titleErr ? 'red' : '#000000' }}
+                                onChangeText={e => updateField({ title: e })}
+                                value={state.title}
+                                errorMessage={state.titleErr}
+                                onFocus={() => updateField({ titleErr: '' })}
+                                leftIcon={
+                                    <FontIcon
+                                        name='slack'
+                                        size={24}
+                                        color='black'
+                                    />
+                                }
+                            />
+
+                            {photo && (
+                                <Image
+                                    source={{ uri: photo.uri }}
+                                    style={{ width: 150, height: 150, marginRight: 10, marginLeft: 10, borderRadius: 250 }}
+                                />
+                            )}
+                            <Button title="Choose Photo" buttonStyle={{ ...loginStyles.loginBtn, width: 150 }} onPress={handleChoosePhoto} />
+                        </View> : state.currentPosition === 1 ? <View>
+                            <Input
+                                placeholder="About my services"
+                                multiline={true}
+                                numberOfLines={4}
+                                inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.aboutService ? 'red' : '#000000' }}
+                                onChangeText={e => updateField({ aboutService: e })}
+                                value={state.aboutService}
+                                errorMessage={state.aboutServiceErr}
+                                onFocus={() => updateField({ aboutServiceErr: '' })}
+                            />
+                            <Input
+                                placeholder="Aboutme"
+                                multiline={true}
+                                numberOfLines={4}
+                                inputContainerStyle={{ ...loginStyles.inputLogin, borderColor: state.aboutMe ? 'red' : '#000000' }}
+                                onChangeText={e => updateField({ aboutMe: e })}
+                                value={state.aboutMe}
+                                errorMessage={state.aboutMeErr}
+                                onFocus={() => updateField({ aboutMeErr: '' })}
+                            />
+                        </View> : state.currentPosition === 2 ? <View>
+
+                        </View> : state.currentPosition === 3 ? <View>
+                            {uploadVideo ? <Video
+                                source={{ uri: uploadVideo.uri }}
+                                style={{ height: Screen.height / 3 }}
+                                resizeMode="contain"
+                                controls={true}
+                                paused={true}
+                                thu
+                            /> : null}
+                            {!photo && (
+                                <View style={{ justifyContent: 'center' }}>
+                                    <TouchableOpacity onPress={() => setShowVideo(true)} style={{ height: 230, width: Screen.width }}>
+                                        <Image
+                                            source={{ uri: 'https://res.cloudinary.com/dhspait8a/image/upload/v1595100989/cwbwopm3ys9hpkjaajw1.jpg' }}
+                                            style={{ height: 230, width: Screen.width, resizeMode: 'cover' }}
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => setShowVideo(true)} style={AdvisorStyles.playButton}>
+                                        <MaterialIcon style={{ color: '#fff' }} name="play-arrow" size={42} />
+                                    </TouchableOpacity>
+                                </View>
+                            )}
+                            <Button title="Record or Choose Video" buttonStyle={{ ...loginStyles.loginBtn, width: 150 }} onPress={handleChooseVideo} />
+                            <Text>Briefly tell potential clients about your service. Make sure that the video and the audio clear and give it your best effort</Text>
+                        </View> : state.currentPosition === 4 ? <View>
+
+                        </View> : null}
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
+                        <Button icon={
+                            <FontIcon
+                                name='arrow-left'
+                                size={24}
+                                color='#fff'
+                            />
+                        }
+                            buttonStyle={{ width: 150 }}
+                            disabled={!state.currentPosition}
+                            onPress={() => updateFieldSteps('left')}
+                        />
+                        <Button icon={
+                            <FontIcon
+                                name='arrow-right'
+                                size={24}
+                                color='#fff'
+                            />
+                        }
+                            buttonStyle={{ width: 150 }}
+                            onPress={() => updateFieldSteps('right')}
+                            disabled={state.currentPosition === 5}
+                        />
+                    </View>
+                </View>}
+        </SafeAreaView>
     );
 };
 
